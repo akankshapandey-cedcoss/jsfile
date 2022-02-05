5a209086c79138ae83cfd78c613f48c66c8d5cfc
akankshapandey-cedcoss <akankshapandey@cedcoss.com>
2022-02-05 11:15:44 +0530

    understanding uninitialized variables

--- src/myscript.js
+++ src/myscript.js
@@ -1,5 +1,13 @@
 // this is a file//
 /* this is not a file */
-var a = 9;
-var myFirstName ="abc";
-var myLastName ="efg";
+var a;
+a=5;
+var b;
+b=10;
+var c;
+c="I am a";
+
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
