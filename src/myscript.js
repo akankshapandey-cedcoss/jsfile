a9d2665a9af3eacfa5746834a1a5f102d5c84d1e
akankshapandey-cedcoss <akankshapandey@cedcoss.com>
2022-02-05 14:51:58 +0530

    global scope and functions

--- src/myscript.js
+++ src/myscript.js
@@ -123,3 +123,29 @@ function functionWithArgs(a,b) {
       }
       
       var answer = timesFive(2);
+      let myGlobal = 10;
+oopsGlobal = 5;
+
+function fun1(myGlobal) {
+  // Assign 5 to oopsGlobal Here
+  var output = "";
+  if (typeof myGLobal != "undefined") {
+output += "myGlobal: " + myGlobal;
+  }
+  if (typeof myGlobal != "undefiend") {
+output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
