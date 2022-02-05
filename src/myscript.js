6a7da0a51e604ca95a390d957d84939cd468796e
akankshapandey-cedcoss <akankshapandey@cedcoss.com>
2022-02-05 15:03:42 +0530

    global vs. local scope in functions

--- src/myscript.js
+++ src/myscript.js
@@ -157,3 +157,13 @@ function myLocalScope() {
     console.log(myVar);
   }
   myLocalScope();
+  const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+const outerWear ="sweater";
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
